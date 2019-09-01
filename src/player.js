3aff79c3c9a7d03ed6cc05a2c18693964ae98b53
LeonardoP98 <53346686+LeonardoP98@users.noreply.github.com>
2019-09-01 19:01:17 +0200

    to implement character

--- src/player.js
+++ src/player.js
@@ -1,7 +1,34 @@
 export default class Player {
-  constructor() {}
+  constructor(game, position) {
+    this.image = document.getElementById("img_player");
+    this.image.setAttribute("src", "/assets/images/characters");
 
-  update() {}
+    this.game = game;
 
-  draw() {}
+    this.position = position;
+
+    this.width = 80;
+
+    this.height = 24;
+
+    this.markedForDeletion = false;
+  }
+
+  update(deltaTime) {
+    if (detectCollision(this.game.ball, this)) {
+      this.game.ball.speed.y = -this.game.ball.speed.y;
+
+      this.markedForDeletion = true;
+    }
+  }
+
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+  }
 }
